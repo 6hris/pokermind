c5df6791f6f7e3b9d0695dfb72f7095066374cbc
klutetr <tdiggity2002@gmail.com>
2025-03-14 17:03:49 -0400

    Added seats when user or models are added to game.

--- pokermindapp/src/App.js
+++ pokermindapp/src/App.js
@@ -6,19 +6,24 @@ import MessageLog from './components/MessageLog';
 import './App.css';
 
 function App() {
+  // Basic states
   const [openRouterKey, setOpenRouterKey] = useState('');
   const [rounds, setRounds] = useState('');
   const [startingAmount, setStartingAmount] = useState('');
   const [playAgainstLLMs, setPlayAgainstLLMs] = useState(true);
 
+  // NEW: Store the array of selected models in the parent
+  const [modelsInGame, setModelsInGame] = useState([]);
+
+  // Called when user hits "Start Game." For now, just logs everything.
   const handleStartGame = () => {
-    console.log('Game started with:', {
+    console.log('Starting game with:', {
       openRouterKey,
       rounds,
       startingAmount,
       playAgainstLLMs,
+      modelsInGame,
     });
-    // Additional logic to actually kick off the game
   };
 
   return (
@@ -28,6 +33,13 @@ function App() {
       </header>
 
       <div className="container">
+        {/*
+          Pass down:
+          1) The states for openRouterKey, rounds, etc.
+          2) The states for whether user is playing.
+          3) The modelsInGame + setModelsInGame so SetupPanel can add or remove models.
+          4) The handleStartGame function.
+        */}
         <SetupPanel
           openRouterKey={openRouterKey}
           setOpenRouterKey={setOpenRouterKey}
@@ -37,10 +49,19 @@ function App() {
           setStartingAmount={setStartingAmount}
           playAgainstLLMs={playAgainstLLMs}
           setPlayAgainstLLMs={setPlayAgainstLLMs}
+          modelsInGame={modelsInGame}
+          setModelsInGame={setModelsInGame}
           onStartGame={handleStartGame}
         />
 
-        <PokerTable />
+        {/*
+          Pass the same "modelsInGame" and "playAgainstLLMs" to PokerTable.
+          That way, it can dynamically render seats for each model.
+        */}
+        <PokerTable
+          models={modelsInGame}
+          userIsPlaying={playAgainstLLMs}
+        />
 
         <MessageLog />
       </div>
